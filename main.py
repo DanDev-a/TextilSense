7ce0271f8a9b0fd5288ff479bd507ed972a938b7
Lobul24 <kalefinley@gmail.com>
2025-04-30 09:35:44 -0400

    Codigo para ejecutar con webcam

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
 #val se tienen imagenes diferentes a train
-#ejemplo: train 1000 imagenes - val 200 imagenes
+#ejemplo: train 1000 imagenes - val 200 imagenes# Importamos las librerias
+from ultralytics import YOLO
+import cv2
+
+# Leer nuestro modelo
+model = YOLO("best.pt")
+
+# Realizar VideoCaptura
+cap = cv2.VideoCapture(0)
+
+# Bucle
+while True:
+    # Leer nuestros fotogramas
+    ret, frame = cap.read()
+
+    # Leemos resultados
+    resultados = model.predict(frame, imgsz = 640, conf = 0.98)
+
+    # Mostramos resultados
+    anotaciones = resultados[0].plot()
+
+    # Mostramos nuestros fotogramas
+    cv2.imshow("DETECCION Y SEGMENTACION", anotaciones)
+
+    # Cerrar nuestro programa
+    if cv2.waitKey(1) == 27:
+        break
+
+        
+
+cap.release()
+cv2.destroyAllWindows()
